fd7a3d2e0b3014b4b821eb8874aa87f6c7fb0613
agatar <agata.rygielska@gmail.com>
2020-04-24 20:39:42 +0200

    added menu i cos tam

---
+++ samplemenu.js
@@ -0,0 +1,58 @@
+
+menuitems = {   
+    "topics": [
+    {
+        "title": "Topik 1",
+        "path": "#topik1",
+        "children": null
+    },
+   
+
+    {
+        "title": "Topik 2",
+        "path": "#topik2",
+        "children": null
+    },
+
+    {
+        "title": "Topik 3",
+        "path": "#topik3",
+        "children": [
+
+        {
+            "title": "Topik Zagnieżdżon",
+            "path": "#topik4",
+            "children": null
+
+        },
+
+        {
+            "title": "Topik Zagnieżdżon 2",
+            "path": "#topik5",
+            "children": [
+
+            {
+                "title": "Topik Wybitnie Zagnieżdżon",
+                "path": "#topik5",
+                "children": null
+
+            }
+            ]
+
+        }
+
+       
+
+        ]
+    
+    },
+    {
+        "title": "Topik Ostatni",
+        "path": "#topik6",
+        "children": null
+    }
+
+]
+}
+
+
